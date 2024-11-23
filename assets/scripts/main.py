3fb9255b3b6786f3a8768ea6ab4579f8d0924efc
unknown <tobiasschmidt711@gmail.com>
2024-11-22 23:37:31 -0300

    Added sprites.py

--- assets/scripts/main.py
+++ assets/scripts/main.py
@@ -1,1 +1,6 @@
 import pygame
+from sprites import *
+
+SX = 800
+SY = 800
+enemy_sprite
---
+++ assets/scripts/sprites.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+
+bg = pygame.image.load("../sprites/background.png")
+
+player_sprite = pygame.image.load("../sprites/triangle.png")
+
+enemy_sprite = pygame.image.load("../sprites/enemy.png")
+
+heal = pygame.image.load("../sprites/star_f.png")
