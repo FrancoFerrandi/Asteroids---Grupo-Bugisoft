a0bfab5334b88a3832e4712d00d329bffdfe3ad7
unknown <tobiasschmidt711@gmail.com>
2024-11-24 11:44:52 -0300

    Updated main.py, Updated sprites.py

--- assets/scripts/main.py
+++ assets/scripts/main.py
@@ -1,23 +1,43 @@
 import pygame
-from sprites import *
 from settings import *
+from sprites import *
+
 
 pygame.display.set_caption("Asteroids")
 
-#para inicializar un delta time
-clock = pygame.time.Clock()
+#para inicializar un delta time 
+clock = pygame.time.Clock() 
 display = pygame.display.set_mode((SX, SY))
 
 
 gg = False
 
+
+class Player(object):
+    def __init__(self):
+        self.img = player_sprite
+        self.w = self.img.get_width()
+        self.h = self.img.get_height()
+        self.x = SX//2
+        self.y = SY//2
+        
+    def draw(self, display):
+        display.blit(self.img, (self.x, self.y, self.w, self.h))
+
+
 def redrawGameWindow():
-    display.blit(bg, (0, 0))
+    display.blit(bg_big, (0, 0))
+    player.draw(display)
     pygame.display.update()
 
 
 run = True
 #bandera para verificar si el juego esta siendo renderizado para saber si debe finalizar el codigo
+
+player = Player()
+
+
+
 while run:
     clock.tick(60)
     if not gg:
--- assets/scripts/sprites.py
+++ assets/scripts/sprites.py
@@ -1,20 +1,41 @@
 import pygame
 
+#scaled_sprite = pygame.transform.scale(sprite, (100, 100))
 
+bg = pygame.image.load("assets/sprites/background.png")
+bg_big = pygame.transform.scale(bg, (1500, 1500))
 
+#jugador y enemigo
 
+player_sprite = pygame.image.load("assets/sprites/triangle.png")
+player_big = pygame.transform.scale(player_sprite, (1500, 1500))
 
-player_sprite = pygame.image.load("../sprites/triangle.png")
-enemy_sprite = pygame.image.load("../sprites/enemy.png")
+enemy_sprite = pygame.image.load("assets/sprites/enemy.png")
+enemy_big = pygame.transform.scale(enemy_sprite, (1500, 1500))
 
-heal_sprite = pygame.image.load("../sprites/star_f.png")
-fakeheal_sprite = pygame.image.load("../sprites/star_e.png")
+#objetos interactivos
 
-bullet_e_sprite = pygame.image.load("../sprites/bullet_e.png")
-bullet_f_sprite = pygame.image.load("../sprites/bullet_f.png")
+heal_sprite = pygame.image.load("assets/sprites/star_f.png")
+heal_big = pygame.transform.scale(heal_sprite, (1500, 1500))
 
-asteroid_s_sprite = pygame.image.load("../sprites/asteroid_s.png")
-asteroid_m_sprite = pygame.image.load("../sprites/asteroid_m.png")
-asteroid_l_sprite = pygame.image.load("../sprites/asteroid_L.png")
+fakeheal_sprite = pygame.image.load("assets/sprites/star_e.png")
+fakeheal_big = pygame.transform.scale(fakeheal_sprite, (1500, 1500))
 
-bg = pygame.image.load("../sprites/background.png")
+#balas
+
+bullet_e_sprite = pygame.image.load("assets/sprites/bullet_e.png")
+bullet_e_big = pygame.transform.scale(bullet_e_sprite, (1500, 1500))
+
+bullet_f_sprite = pygame.image.load("assets/sprites/bullet_f.png")
+bullet_f_big = pygame.transform.scale(bullet_f_sprite, (1500, 1500))
+
+#asteroides
+
+asteroid_s_sprite = pygame.image.load("assets/sprites/asteroid_s.png")
+asteroid_s_big = pygame.transform.scale(asteroid_s_sprite, (1500, 1500))
+
+asteroid_m_sprite = pygame.image.load("assets/sprites/asteroid_m.png")
+asteroid_m_big = pygame.transform.scale(asteroid_m_sprite, (1500, 1500))
+
+asteroid_l_sprite = pygame.image.load("assets/sprites/asteroid_L.png")
+asteroid_l_big = pygame.transform.scale(asteroid_l_sprite, (1500, 1500))
